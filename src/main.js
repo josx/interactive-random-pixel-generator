2796227ce384f77b47f3376bb9f7c410ca7b4c87
Damian Mantuano <damianm@camba.coop>
2018-06-07 18:20:35 -0300

    [DM] Imported vuetify and materialDesign icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import materialDesignIconsIconfont from 'material-design-icons-iconfont'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
+Vue.use(materialDesignIconsIconfont)
 
 new Vue({
   render: h => h(App)
