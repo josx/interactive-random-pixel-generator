ab5917d95e3d8349c899b41b406801015b81af16
Damian Mantuano <damianm@camba.coop>
2018-06-07 18:22:29 -0300

    [DM] Set new layout to randomCanvas component.

--- src/components/RandomCanvas.vue
+++ src/components/RandomCanvas.vue
@@ -1,57 +1,127 @@
 <template>
-  <div id="random-canvas">
-    <Canvas :pixel-data="pixelData" :colors="colors" :pixel-size="pixelSize" :background="background"></Canvas>
-    <input type="radio" id="bw" value="bw" v-model="palette">
-    <label for="bw">Black and White</label>
-    <br>
-    <input type="radio" id="rgb" value="rgb" v-model="palette">
-    <label for="rgb">rgb</label>
-    <br>
-    <input type="radio" id="mario" value="mario" v-model="palette">
-    <label for="mario">mario</label>
-    <br>
-    <input type="radio" id="luigi" value="luigi" v-model="palette">
-    <label for="luigi">luigi</label>
-    <br>
-    <input type="radio" id="monalisa" value="monalisa" v-model="palette">
-    <label for="monalisa">monalisa</label>
-    <br>
-    <label for="pixel">Pixel Size</label>
-    <input v-model.number="pixelSizeNumber" type="number">
-    <br>
+  <v-app id="random-canvas">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-subheader>Palette</v-subheader>
+        <div class="ml-3 mr-3">
+          <v-switch
+            id="byn"
+            value="byn"
+            :label="`Black and White`"
+            v-model="palette"
+          ></v-switch>
+          <v-switch
+            id="rgb"
+            value="rgb"
+            :label="`RGB`"
+            v-model="palette"
+          ></v-switch>
+          <v-switch
+            id="mario"
+            value="mario"
+            :label="`Mario`"
+            v-model="palette"
+          ></v-switch>
+          <v-switch
+            id="luigi"
+            value="luigi"
+            :label="`Luigi`"
+            v-model="palette"
+          ></v-switch>
+          <v-switch
+            id="monalisa"
+            value="monalisa"
+            :label="`Monalisa`"
+            v-model="palette"
+          ></v-switch>
+        </div>
+        <v-divider></v-divider>
+        <v-subheader>Pixel</v-subheader>
+        <div class="ml-3 mr-3">
+          <v-text-field
+            v-model.number="pixelSizeNumber"
+            name="input-2"
+            label="Pixel Size"
+            value="Input text"
+            class="input-group--focused"
+            type="number"
+          ></v-text-field>
+        </div>
+        <v-divider></v-divider>
+        <v-subheader>Canvas</v-subheader>
+        <div class="ml-3 mr-3">
+          <v-text-field
+            v-model.number="imageSizeWidth"
+            name="input-2"
+            label="Image size width"
+            value="Input text"
+            class="input-group--focused"
+            type="number"
+          ></v-text-field>
+          <v-text-field
+            v-model.number="imageSizeHeight"
+            name="input-2"
+            label="ImageSize height"
+            value="Input text"
+            class="input-group--focused"
+            type="number"
+          ></v-text-field>
+        </div>
+        <v-divider></v-divider>
+        <div  class="pt-2 pl-5 pr-5">
 
-    <label for="pixel">Image size width</label>
-    <input v-model.number="imageSizeWidth" type="number">
-    <br>
-     <label for="pixel">ImageSize height</label>
-    <input v-model.number="imageSizeHeight" type="number">
-    <br>
-
-    <button v-on:click="clear">Clear</button>
-    <button v-on:click="renderImage">Render</button>
-    <button v-on:click="showMario">Mario</button>
-    <button v-on:click="showLuigi">Luigi</button>
-  </div>
+          <v-btn centered color="success" block v-on:click="renderImage">Render</v-btn>
+        </div>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex text-xs-center>
+              <v-card>
+                <Canvas :pixel-data="pixelData" :colors="colors" :pixel-size="pixelSize" :background="background"></Canvas>
+              </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <v-btn color="error" v-on:click="clear" flat  block>Clear</v-btn>
+      <v-btn color="success" v-on:click="renderImage"  block>Render</v-btn>
+      <!-- <v-btn color="error"  v-on:click="showMario">Mario</v-btn>
+      <v-btn color="error"  v-on:click="showLuigi">Luigi</v-btn> -->
+    </v-footer>
+  </v-app>
 </template>
-
 <script>
+
 import Canvas from './Canvas'
 import { Mario, RandomImage, Luigi, DefaultImage } from '../utils'
 
 export default {
   name: 'RandomCanvas',
-  props: {
-  },
   components: {
     Canvas
   },
   data() {
     return {
       ...DefaultImage,
-      palette: '',
+      palette: 'byn',
       pixelSizeNumber: 20,
       imageSizeWidth: 14,
-      imageSizeHeight: 17
+      imageSizeHeight: 17,
+      drawer: null
     }
   },
   created() {
